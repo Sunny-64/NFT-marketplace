46e19f2064c942d46cc28a899a54c869cb83e49c
Sunny <sunny6464n@gmail.com>
2023-09-07 16:54:03 +0530

    fix : show auction button in search if nft is auctioned

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import web3 from './../scripts/web3'
+import initWeb3 from './../scripts/web3'
 import { initContract } from "./../scripts/contract";
 import ApiService from '../services/ApiServices';
 
@@ -10,16 +10,26 @@ import { CSSProperties } from "react";
 import MoonLoader from "react-spinners/MoonLoader";
 
 import web3Utils from '../scripts/web3Utils';
+import { useNavigate } from 'react-router-dom';
 
 function Search() {
-  const [nfts, setNfts] = useState([]); 
-  const [contract, setContract] = useState({}); 
+  const [nfts, setNfts] = useState([]);
+  const [contract, setContract] = useState({});
 
-  const [categories, setCategories] = useState([]); 
-  const [name, setName] = useState(""); 
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
   let [loading, setLoading] = useState(false);
-
+  const [web3, setWeb3] = useState({});
+  const navigate = useNavigate(); 
   useEffect(() => {
+    initWeb3()
+      .then(web3Instance => {
+        setWeb3(web3Instance);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
     initContract()
       .then((contractInstance) => {
         if (contractInstance) {
@@ -36,30 +46,30 @@ function Search() {
         console.log(err);
       });
 
-      const fetchData = async () => {
-        // fetch categories
-        setLoading(true); 
-        const fetchCategories = await ApiService.getCategories(); 
-        // console.log(fetchCategories.data.data);
-        setCategories(fetchCategories.data.data); 
-        setLoading(false); 
-      }
+    const fetchData = async () => {
+      // fetch categories
+      setLoading(true);
+      const fetchCategories = await ApiService.getCategories();
+      // console.log(fetchCategories.data.data);
+      setCategories(fetchCategories.data.data);
+      setLoading(false);
+    }
 
-      fetchData(); 
+    fetchData();
 
   }, [])
   // console.log(categories);
 
-  const filterByCategory = async (category) =>{
-      setLoading(true); 
-      const data = await ApiService.searchByCategory(category); 
-      console.log(data);
-      setNfts(data.data.data);
-      setLoading(false)
+  const filterByCategory = async (category) => {
+    setLoading(true);
+    const data = await ApiService.searchByCategory(category);
+    console.log(data);
+    setNfts(data.data.data);
+    setLoading(false)
   }
 
   const purchaseNFT = async (index, price) => {
-    
+
     initContract()
       .then((contractInstance) => {
         if (contractInstance) {
@@ -80,14 +90,14 @@ function Search() {
       return toast.error("You have to login to purchase the NFT");
     }
     try {
-      setLoading(true); 
+      setLoading(true);
       const accounts = await web3.eth.getAccounts();
-     
+
       const purchase = await contract.methods.purchaseNFT(index).send({
         from: accounts[0],
         value: price
       });
-      
+
       // console.log("tx", purchase);
       setLoading(false);
     }
@@ -97,15 +107,15 @@ function Search() {
   }
 
   const handleSearchNFt = async (e) => {
-      e.preventDefault(); 
-      if(name.trim() === ""){
-        return; 
-      }
-      setLoading(true); 
-      const data = await ApiService.searchByName(name); 
-      // console.log(data);
-      setNfts(data.data.data); 
-      setLoading(false); 
+    e.preventDefault();
+    if (name.trim() === "") {
+      return;
+    }
+    setLoading(true);
+    const data = await ApiService.searchByName(name);
+    // console.log(data);
+    setNfts(data.data.data);
+    setLoading(false);
   }
   const override = {
     margin: "0 auto",
@@ -113,24 +123,24 @@ function Search() {
     position: "absolute",
     // width : "", 
     top: "40%",
-    left : "45%",
+    left: "45%",
     marginLeft: "auto",
     display: "block",
   };
   return (
 
     <>
-     {/* <div className='relative w-full flex justify-center z-100'> */}
-        <MoonLoader
-          color={"#ffffff"}
-          loading={loading}
-          cssOverride={override}
-          size={60}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
+      {/* <div className='relative w-full flex justify-center z-100'> */}
+      <MoonLoader
+        color={"#ffffff"}
+        loading={loading}
+        cssOverride={override}
+        size={60}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
       {/* </div> */}
-    <ToastContainer
+      <ToastContainer
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
@@ -142,51 +152,67 @@ function Search() {
         pauseOnHover
         theme="dark"
       />
-    <div className='search flex justify-center gap-5'>
-      <div className="">
-        <form action="" onSubmit={handleSearchNFt}>
-          <input className='px-2 py-1 rounded-sm w-[400px]' style={{color : "black"}} type="text" placeholder='Search for NFT' name='search' value={name} onChange={(e) => setName(e.target.value)} />
-          <button type='submit' className='btn-primary px-2 py-1 rounded-sm'>Search</button>
-        </form>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn px-3 py-1">Categories</button>
-        <div className="dropdown-content">
-          {
+      <div className='search flex justify-center gap-5'>
+        <div className="">
+          <form action="" onSubmit={handleSearchNFt}>
+            <input className='px-2 py-1 rounded-sm w-[400px]' style={{ color: "black" }} type="text" placeholder='Search for NFT' name='search' value={name} onChange={(e) => setName(e.target.value)} />
+            <button type='submit' className='btn-primary px-2 py-1 rounded-sm'>Search</button>
+          </form>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn px-3 py-1">Categories</button>
+          <div className="dropdown-content">
+            {
               categories.map((category, index) => {
-                  return (
-                    <p onClick={() => filterByCategory(category)}>{category}</p>
-                  )
+                return (
+                  <p onClick={() => filterByCategory(category)}>{category}</p>
+                )
               })
-          }
+            }
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="nfts px-6 mt-20">
-    {nfts?.map((item, index) => {
-          {/* console.log(item) */}
+      <div className="nfts px-6 mt-20">
+        {nfts?.map((item, index) => {
+          {/* console.log(item) */ }
           return (
             <>
-              <div key={index} className='card bg-[#343444] w-[320px] rounded-lg px-4 py-2 shadow-sm shadow-[#79279F] mb-6'>
-                <p>name : {item.name}</p>
-                <p className='break-words'>Description : {item.description}</p>
-                <img src={item.tokenURI} className='w-full rounded-md my-3 h-[250px] object-cover' alt='' />
-                <div className='flex justify-between'>
-                  <p>Item Id : {item.tokenId}</p>
-                  <p>Price : {Number(web3Utils.fromWei(item.price, "ether"))}</p>
+              {!item?.isListedForAuction ?
+                <div key={index} className='card bg-[#343444] w-[320px] rounded-lg px-4 py-2 shadow-sm shadow-[#79279F] mb-6'>
+                  <p>name : {item.name}</p>
+                  <p className='break-words'>Description : {item.description}</p>
+                  <img src={item.tokenURI} className='w-full rounded-md my-3 h-[250px] object-cover' alt='' />
+                  <div className='flex justify-between'>
+                    <p>Item Id : {item.tokenId}</p>
+                    <p>Price : {Number(web3Utils.fromWei(item.price, "ether"))}</p>
+                  </div>
+                  <div className='flex justify-between items-center mt-3'>
+                    <p>Category : {item.category}</p>
+                    {item.price > 0 && <button className='py-2 rounded-md btn-primary px-3' onClick={() => purchaseNFT(index, item.price)}>Purchase</button>}
+                  </div>
                 </div>
-                <div className='flex justify-between items-center mt-3'>
-                  <p>Category : {item.category}</p>
-                  {item.price > 0 && <button className='py-2 rounded-md btn-primary px-3' onClick={() => purchaseNFT(index, item.price)}>Purchase</button>}
+                :
+                <div key={index} className='card bg-[#343444] w-[320px] rounded-lg px-4 py-2 shadow-sm shadow-[#79279F] mb-6'>
+                  <p>name : {item.name}</p>
+                  <p className='break-words'>Description : {item.description}</p>
+                  <img src={item.tokenURI} className='w-full rounded-md my-3 h-[250px] object-cover' alt='' />
+                  <div className='flex justify-between'>
+                    <p>Item Id : {item.tokenId}</p>
+                    <p>Price : {Number(web3Utils.fromWei(item.price, "ether"))}</p>
+                  </div>
+                  <div className='flex justify-between items-center mt-3'>
+                    <p>Category : {item.category}</p>
+                    {item.price > 0 && <button className='py-2 rounded-md btn-primary px-3' onClick={() => navigate("/")}>Auction</button>}
+                  </div>
                 </div>
-              </div>
+              }
             </>
           )
         })}
-    </div>
+      </div>
 
-    
+
     </>
   )
 }
